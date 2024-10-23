fa5e12d063a268347cc94cd859ef10a77d659b77
keithpohl82 <keith.pohl82@gmail.com>
2024-10-23 13:56:53 -0500

    completed all test are passing

--- queries.sql
+++ queries.sql
@@ -1,16 +1,19 @@
 --Part 1
--- id int
--- employer varchar(255)
--- name varchar(255)
--- skills varchar(255)
+ id int
+ employer varchar(255)
+ name varchar(255)
+ skills varchar(255)
 
 --Part 2
--- SELECT name FROM employer WHERE location = "St. Louis City";
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
--- DROP TABLE job;
+DROP TABLE job;
 
 --Part 4
--- SELECT * FROM skill INNER JOIN job_skills ON (skill.id = job_skills.skills_id|skill.id = job_skills.skills_id) (WHERE job_skills.jobs_id IS NOT NULL) ORDER BY name ASC;
-
+ SELECT *
+ FROM skill
+ INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ WHERE job_skills.jobs_id IS NOT NULL
+ ORDER BY name ASC;
 
