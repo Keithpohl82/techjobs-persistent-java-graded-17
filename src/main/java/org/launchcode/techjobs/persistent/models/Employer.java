11605987c29682d3ad6fb2ab7ff3c5c286d2ec2c
keithpohl82 <keith.pohl82@gmail.com>
2024-10-23 09:43:25 -0500

    start of task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    public String location;
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
